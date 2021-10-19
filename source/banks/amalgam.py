7168e9882f67589d51ebe290c77fba307363dd7d
Timothy Lee <lee.timothy116@gmail.com>
2021-10-19 20:18:47 +1100

    bring in functions not required in other file

--- source/banks/amalgam.py
+++ source/banks/amalgam.py
@@ -4,17 +4,28 @@ pd.options.display.max_columns = 1000
 pd.options.display.max_rows = 1000
 
 from source import accounts
-from source.utilities.amalgam.functions import parse_transactions
 
-up = accounts.Up(amalgam=True)
-up.transactions.df['source'] = 'up'
-ing = accounts.ING(amalgam=True) # Assumed all values within are provided at settledAt rather than createdAt - see accounts.ING for more info
-ing.transactions.df['source'] = 'ing'
+def parse_transactions(df):
+    df = df.sort_values('attributes_settledAt', ascending = False).reset_index(drop=True)
+    df['month'] = df['attributes_settledAt'].dt.month
+    df['year'] = df['attributes_settledAt'].dt.year
+    df['month_year'] = df['attributes_settledAt'].dt.to_period('M')
+    return df
 
-accounts = pd.concat([up.accounts.df,ing.accounts.df])
+def tag_transactions(df):
+    return
 
-transactions = pd.concat([up.transactions.df, ing.transactions.df])
-transactions = parse_transactions(transactions)
+class Banks(object):
+
+    def __init__(self):
+        self.up = accounts.Up(amalgam=True)
+        self.ing = accounts.ING(amalgam=True) # Assumed all values within are provided at settledAt rather than createdAt - see accounts.ING for more info
+        self.accounts = pd.concat([self.up.accounts.df,self.ing.accounts.df])
+        self.transactions = pd.concat([self.up.transactions.df, self.ing.transactions.df])
+        self.transactions = parse_transactions(self.transactions)
+        self.up = self.up.transactions.df
+        self.ing = self.ing.transactions.df
+        return
 
 
 
