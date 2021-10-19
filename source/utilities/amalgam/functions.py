8ee51595255d98e36fb38efc9c5409b36475024e
Timothy Lee <lee.timothy116@gmail.com>
2021-10-19 20:19:38 +1100

    rm redudanct dir and file

--- source/utilities/amalgam/functions.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def parse_transactions(df):
-    df = df.sort_values('attributes_settledAt', ascending = False).reset_index(drop=True)
-    df['month'] = df['attributes_settledAt'].dt.month
-    df['year'] = df['attributes_settledAt'].dt.year
-    df['month_year'] = df['attributes_settledAt'].dt.to_period('M')
-    return df
